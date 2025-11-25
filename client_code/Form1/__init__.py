8dd70c8408783d0654b63dd54621b953a7cd3391
a.giesbrecht13@outlook.com <a.giesbrecht13@outlook.com>
2025-11-25 23:56:53 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import time
+import random
 
 
 class Form1(Form1Template):
@@ -8,3 +13,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    for i in range(0, random.randint(6, 10)):
+      print(i)
+      time.sleep(random.random() + random.randint(0, 1))
+      
